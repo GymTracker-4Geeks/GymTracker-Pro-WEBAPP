cc08d7f3bb996c91247bd772b1630be3e25f9d0d
DeividEA <evoradavid17@gmail.com>
2026-06-05 20:04:04 +0200

    feat: frontend folder structure updated, backend folder structure added, branches templated done

---
+++ backend/app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from extensions import db, cors, jwt
+from config import Config
+
+app = Flask("app")
+app.config.from_object(Config)
+
+db.init_app(app)
+jwt.init_app(app)
+cors.init_app(app, origins=["http://localhost:5173"])
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
