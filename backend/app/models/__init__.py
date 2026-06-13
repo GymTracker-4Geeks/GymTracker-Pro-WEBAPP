2980a2f2b193b026a7b9de4b2c6deef3fae0616b
DeividEA <evoradavid17@gmail.com>
2026-06-13 19:51:51 +0200

    feat: main branch empty

--- backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .user import User
-from .trainer import Trainer
-from .client import Client
-from .routine import Routine
-from .exercise import Exercise
-from .workout_log import WorkoutLog
-from .body_weight import BodyWeight
